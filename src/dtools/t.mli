6957fc9197461e38e7e9e98733dffd1cd6075d0f
Stéphane Gimenez <gimenez@pps.jussieu.fr>
2009-02-26 12:46:35 +0000

    - del: removed src/dtools for merge_into_dir operation.

--- src/dtools/t.mli
+++
@@ -1,8 +0,0 @@
-type mutex = < lock: unit; unlock: unit; >
-
-val mutex : unit -> mutex
-
-val exec : (unit -> unit) list -> unit
-
-val mutex_ref : (unit -> mutex) ref
-val exec_ref : ((unit -> unit) list -> unit) ref
