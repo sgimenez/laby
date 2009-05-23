0c59d9fe3bed3b9da8301d96e06e985fac74c982
Stéphane Gimenez <gimenez@pps.jussieu.fr>
2009-05-23 01:21:42 +0000

    - chg: log message cosmetic.

--- res.ml
+++ res.ml
@@ -61,9 +61,9 @@ let get rpath =
   in
   let found x =
     log#debug 2 (
-      F.x "found resource <resource> at <location>" [
+      F.x "found resource <resource> at: <location>" [
 	"resource", F.string resource;
-	"location", F.string x;
+	"location", F.q (F.string x);
       ]
     );
   in
