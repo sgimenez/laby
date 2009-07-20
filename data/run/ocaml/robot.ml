798b74491c22568c66d578db101b3ead55afcd65
Stéphane Gimenez <gimenez@pps.jussieu.fr>
2009-07-20 12:56:07 +0000

    - chg: minor simplifications in ocaml mode.

--- data/run/ocaml/robot.ml
+++ data/run/ocaml/robot.ml
@@ -1,3 +1,10 @@
+type tile =
+  | laby_name_Void
+  | laby_name_Wall
+  | laby_name_Rock
+  | laby_name_Web
+  | laby_name_Exit
+
 let input_ch, output_ch =
   stdin, stdout
 
@@ -11,27 +18,34 @@ let input () =
     | End_of_file -> exit 1
   end
 
-let laby_name_left () = output "left"; ignore (input ())
+let laby_name_left () =
+  output "left"; ignore (input ())
 
-let laby_name_right () = output "right"; ignore (input ())
+let laby_name_right () =
+  output "right"; ignore (input ())
 
-let laby_name_forward () = output "forward"; ignore (input ())
+let laby_name_forward () =
+  output "forward"; ignore (input ())
 
-let laby_name_look () = output "look";
+let laby_name_look () =
+  output "look";
   begin match input () with
-  | "void" -> `laby_name_Void
-  | "wall" -> `laby_name_Wall
-  | "rock" -> `laby_name_Rock
-  | "web" -> `laby_name_Web
-  | "exit" -> `laby_name_Exit
+  | "void" -> laby_name_Void
+  | "wall" -> laby_name_Wall
+  | "rock" -> laby_name_Rock
+  | "web" -> laby_name_Web
+  | "exit" -> laby_name_Exit
   | s -> Printf.eprintf "robot: unknown tile : %s\n" s; assert false
   end
 
-let laby_name_door_open () = output "open"; ignore (input ())
+let laby_name_door_open () =
+  output "open"; ignore (input ())
 
-let laby_name_take () = output "take"; ignore (input ())
+let laby_name_take () =
+  output "take"; ignore (input ())
 
-let laby_name_drop () = output "drop"; ignore (input ())
+let laby_name_drop () =
+  output "drop"; ignore (input ())
 
 let _ =
   output "start"; ignore (input ())
