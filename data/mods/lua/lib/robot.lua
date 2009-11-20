78a217d3db6ab3d0060feeee76c9d3fa25f35811
Stéphane Gimenez <gimenez@pps.jussieu.fr>
2009-11-20 23:49:06 +0000

    - cln: some cosmetic changes in mods.

--- data/mods/lua/lib/robot.lua
+++ data/mods/lua/lib/robot.lua
@@ -3,31 +3,31 @@ function output(s)
   io.flush();
 end
 
-function input ()
+function input()
     return io.read();
 end
 
-function laby_name_left ()
+function laby_name_left()
   output("left"); input();
 end
 
-function laby_name_right ()
+function laby_name_right()
   output("right"); input();
 end
 
-function laby_name_forward ()
+function laby_name_forward()
   output("forward"); input();
 end
 
-function laby_name_take ()
+function laby_name_take()
   output("take"); input();
 end
 
-function laby_name_drop ()
+function laby_name_drop()
   output("drop"); input();
 end
 
-function laby_name_escape ()
+function laby_name_escape()
   output("escape"); input();
 end
 
@@ -35,14 +35,14 @@ function laby_name_say(s)
   output("say " .. s); input();
 end
 
-laby_name_Void  = 0;
-laby_name_Wall  = 1;
-laby_name_Rock  = 2;
-laby_name_Web  = 3;
-laby_name_Exit  = 4;
-laby_name_Unknown  = 5;
+laby_name_Void = 0;
+laby_name_Wall = 1;
+laby_name_Rock = 2;
+laby_name_Web = 3;
+laby_name_Exit = 4;
+laby_name_Unknown = 5;
 
-function laby_name_look(s)
+function laby_name_look()
   output("look");
   local ans = input();
   if (ans == "void") then return laby_name_Void end;
