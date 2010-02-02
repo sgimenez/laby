d57c0566eab435167f15068de4aeea8b8dd8d40d
Stéphane Gimenez <gimenez@pps.jussieu.fr>
2010-02-02 12:55:39 +0000

    - upd: 2010 copyrights.

--- src/dtools/conf.ml
+++ src/dtools/conf.ml
@@ -1,6 +1,6 @@
 
 (*
-   Copyright (C) 2007-2009 Stéphane Gimenez
+   Copyright (C) 2007-2010 Stéphane Gimenez
    You have permission to copy, modify, and redistribute under the
    terms of the GPL-3.0. For full license terms, see gpl-3.0.txt.
 *)
