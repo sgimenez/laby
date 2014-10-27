e6d783468b6d1273e6c7b40015367d81467c0205
Stéphane Gimenez <gimenez@pps.jussieu.fr>
2014-10-27 10:06:49 +0100

    Release 0.6.4

--- src/trace.mli
+++ src/trace.mli
@@ -1,5 +1,5 @@
 (*
- * Copyright (C) 2007-2010 The laby team
+ * Copyright (C) 2007-2014 The laby team
  * You have permission to copy, modify, and redistribute under the
  * terms of the GPL-3.0. For full license terms, see gpl-3.0.txt.
  *)
